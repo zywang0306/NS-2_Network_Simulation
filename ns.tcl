bdfd5474920976c2a7a2c2b8bba306f87b88b951
zywang0306 <43312614+zywang0306@users.noreply.github.com>
2018-12-05 21:40:29 -0600

    Update ns.tcl

--- ns.tcl
+++ ns.tcl
@@ -1,8 +1,9 @@
+
 set ns [new Simulator]
 set nf [open out.nam w]
 set f1 [open out1.tr w]
 set f2 [open out2.tr w]
-$ns trace-all $nf
+$ns namtrace-all $nf
 $ns namtrace-all $nf
 set src1 [$ns node]
 set src2 [$ns node]
@@ -13,17 +14,52 @@ set rcv2 [$ns node]
 set thruput1 0
 set thruput2 0
 set counter 0
-global delay
-set delay "12.5ms"
-set flav "Sack1"
+
+if { $argc!= 2 } {
+	puts "Please try again."
+	puts "\t\tns2.tcl <TCP_flavor> <case_no>\t\t"
+} else {
+	set TCP_flavor [lindex $argv 0]
+	set case_no [lindex $argv 1]
+	puts "The TCP_flavor is $TCP_flavor"
+	puts "Given case_no = $case_no"
+}
 
 $ns duplex-link $R1 $R2 1.0Mb 5ms DropTail
-$ns duplex-link $src1 $R1 10.0Mb 5ms DropTail  
-$ns duplex-link $rcv1 $R2 10.0Mb 5ms DropTail  
-$ns duplex-link $src2 $R1 10.0Mb $delay DropTail  
-$ns duplex-link $rcv2 $R2 10.0Mb $delay DropTail
-set tcp1 [new Agent/TCP/$flav]
-set tcp2 [new Agent/TCP/$flav]
+
+if {$case_no == 1 } {
+	$ns duplex-link $src1 $R1 10.0Mb 5ms DropTail  
+	$ns duplex-link $rcv1 $R2 10.0Mb 5ms DropTail  
+	$ns duplex-link $src2 $R1 10.0Mb 12.5ms DropTail  
+	$ns duplex-link $rcv2 $R2 10.0Mb 12.5ms DropTail
+} elseif {$case_no == 2 } {
+	$ns duplex-link $src1 $R1 10.0Mb 5ms DropTail  
+	$ns duplex-link $rcv1 $R2 10.0Mb 5ms DropTail  
+	$ns duplex-link $src2 $R1 10.0Mb 20ms DropTail  
+	$ns duplex-link $rcv2 $R2 10.0Mb 20ms DropTail
+} elseif {$case_no == 3 } {
+	$ns duplex-link $src1 $R1 10.0Mb 5ms DropTail  
+	$ns duplex-link $rcv1 $R2 10.0Mb 5ms DropTail  
+	$ns duplex-link $src2 $R1 10.0Mb 27.5ms DropTail  
+	$ns duplex-link $rcv2 $R2 10.0Mb 27.5ms DropTail
+} else {
+	puts "Only input case_no from 1 to 3"
+}
+
+puts "The selected case is $case_no"
+
+if { $TCP_flavor == "VEGAS" } {
+	set tcp1 [new Agent/TCP/Vegas]
+	set tcp2 [new Agent/TCP/Vegas]
+	puts "The flavor is Vegas"
+} elseif { $TCP_flavor == "SACK" } {
+	set tcp1 [new Agent/TCP/Sack1]
+	set tcp2 [new Agent/TCP/Sack1]
+	puts "The flavor is Sack1"
+} else {
+	puts "Correct the flavor"
+}
+
 $ns attach-agent $src1 $tcp1
 $ns attach-agent $src2 $tcp2
 $tcp1 set class_ 1
@@ -40,35 +76,37 @@ $ns connect $tcp1 $sink1
 $ns connect $tcp2 $sink2
 
 proc finish {} {
-global ns nf
+global ns nf tracefile namfile file thruput1 thruput2 counter
 $ns flush-trace
+puts "Avg throughput for Src1=[expr $thruput1/$counter] MBits/sec\n"
+puts "Avg throughput for Src2=[expr $thruput2/$counter] MBits/sec\n"
 close $nf
 exec nam out.nam &
 exit 0
 }
 proc record {} {
 # writes the data to the output files
-global sink1 sink2 f1 f2 thruput1 thruput2 counter
+	global sink1 sink2 f1 f2 thruput1 thruput2 counter
 # get an instance of the simulator
-set ns [Simulator instance]
+	set ns [Simulator instance]
 # set the time after which the procedure would be called again
-set time 0.5
+	set time 0.5
 # how many bytes have been received by the traffic sinks?
-set bw1 [$sink1 set bytes_]
-set bw2 [$sink2 set bytes_]
+	set bw1 [$sink1 set bytes_]
+	set bw2 [$sink2 set bytes_]
 # get the current time
-set now [$ns now]
+	set now [$ns now]
 # calculate bandwidth in Mbit/s and write it to the files
-puts $f1 "$now [expr $bw1/$time*8/1000000]"
-puts $f2 "$now [expr $bw2/$time*8/1000000]"
-set thruput1 [expr $thruput1+ $bw1/$time*8/1000000 ]
-set thruput2 [expr $thruput2+ $bw2/$time*8/1000000 ]
-set counter [expr $counter + 1]
+	puts $f1 "$now [expr $bw1/$time*8/1000000]"
+	puts $f2 "$now [expr $bw2/$time*8/1000000]"
+	set thruput1 [expr $thruput1+ $bw1/$time*8/1000000 ]
+	set thruput2 [expr $thruput2+ $bw2/$time*8/1000000 ]
+	set counter [expr $counter + 1]
 # reset the bytes_ values on the traffic sinks
-$sink1 set bytes_ 0
-$sink2 set bytes_ 0
+	$sink1 set bytes_ 0
+	$sink2 set bytes_ 0
 #Re-schedule the procedure
-$ns at [expr $now+$time] "record"
+	$ns at [expr $now+$time] "record"
 }
 $ns at 0 "record"
 $ns at 0 "$ftp1 start"
